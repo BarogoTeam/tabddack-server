0ccf6c5e13ee5d542e3e84b6176e0c521e6d3b7e
MJ <pingukim@naver.com>
2019-11-09 15:59:34 +0900

    특정 서머너의 accountId 를 가져오는 API

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,21 @@ let conf = require('./../conf.json')
 let express = require('express');
 let router = express.Router();
 
-router.get('/getChallenger', (req, res, next) => {
+router.get('/challengers', (req, res, next) => {
   GET('/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5', function(error, response, body){
       let info_jason = JSON.parse(body);
       let key = Object.keys(info_jason);
       res.send(info_jason);
   })
+})
+
+router.get('/summonerInfo/:summonerId', (req, res, next) => {
+    var summornerId = req.params.summonerId;
+    GET('/summoner/v4/summoners/' + summornerId, function(error, response, body) {
+        let info_jason = JSON.parse(body);
+        let key = Object.keys(info_jason);
+        res.send(info_jason);
+    })
 })
  
 module.exports = router;
