2a3c827706af017edfa29bd6f50f70c0f5265672
RAN <93rann@gmail.com>
2019-11-16 18:20:22 +0900

    매치 타임라인에서 스킬 트리 리턴하는 함수 추가

--- services/matchService.js
+++ services/matchService.js
@@ -1,4 +1,4 @@
-export function parseParticipants(matchData) {
+export function getParticipants(matchData) {
     const getLane = (lane, role) => {
         if (lane !== "BOTTOM") return lane;
         return role === "DUO_CARRY" ? "AD_CARRY" : "SURPPORT";
@@ -13,4 +13,24 @@ export function parseParticipants(matchData) {
     });
 
     return data;
+}
+
+export function getSkillTree(timelineData, participantId){
+    let skillTree = "";
+    let eventList = [];
+
+    timelineData.frames.forEach((f) => {
+        eventList = eventList.concat(f.events);
+    });
+
+    const skillEvents = eventList.filter((e) => {
+        return e.type === "SKILL_LEVEL_UP" && e.participantId === participantId;
+    });
+
+    const skillList = ["Q", "W", "E", "R"];
+    skillEvents.forEach((e) => {
+        skillTree += skillList[e.skillSlot - 1];
+    });
+
+    return skillTree;
 }
