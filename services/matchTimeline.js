b90518c17a6625137d2949d217277a66d8735faf
MJ <pingukim@naver.com>
2019-12-07 14:53:53 +0900

    타임라인에서 아이템 빌드 가져오기(덜함)

--- services/matchTimeline.js
+++ services/matchTimeline.js
@@ -15,24 +15,49 @@ export const getItemBuild = (matchTimelineInfo) => {
         let events = frame.events;
         for (let event of events) {
             if (event.type === 'ITEM_PURCHASED') {
+                console.log(event.participantId + ' / ' + event.type + ' / ' + event.itemId + '(' + gameinfo.getItem(event.itemId).name + ')');
                 if (event.timestamp <= '60000') {
                     participants[event.participantId].startItem.push(event.itemId);
                 }
-                if (1) { // 코어템이면 itemBuild에 push
+                if (1) { // 코어템이면 itemBuild에 push 하도록 변경
                     participants[event.participantId].itemBuild.push(event.itemId);
                 }
-            } else if (event.types === 'ITEM_UNDO') {
-
+            } else if (event.type === 'ITEM_DESTROYED' || event.type === 'ITEM_SOLD') {
+                console.log(event.participantId + ' / ' + event.type + ' / ' + event.itemId + '(' + gameinfo.getItem(event.itemId).name + ')');
+                if (event.timestamp <= '60000') {
+                    let startItemIndex = participants[event.participantId].startItem.indexOf(event.itemId);
+                    participants[event.participantId].startItem.splice(startItemIndex, 1);
+                }
+                let itemIndex = participants[event.participantId].itemBuild.indexOf(event.itemId);
+                participants[event.participantId].itemBuild.splice(itemIndex, 1);
+            } else if (event.type === 'ITEM_UNDO') {
+                console.log(event.participantId + ' / ' + event.type + ' / ' + event.afterId + '(' + gameinfo.getItem(event.afterId).name + ')' + ' / ' + event.beforeId + '(' + gameinfo.getItem(event.beforeId).name + ')');
+                let itemIndex;
+                if(event.afterId !== 0) {
+                    if (event.timestamp <= '60000') {
+                        participants[event.participantId].startItem.push(event.afterId);
+                    }
+                    participants[event.participantId].itemBuild.push(event.afterId);
+                }
+                if(event.beforeId !== 0) {
+                    if (event.timestamp <= '60000') {
+                        itemIndex = participants[event.participantId].startItem.indexOf(event.beforeId);
+                        participants[event.participantId].startItem.splice(itemIndex, 1);
+                    }
+                    itemIndex = participants[event.participantId].itemBuild.indexOf(event.beforeId);
+                    participants[event.participantId].itemBuild.splice(itemIndex, 1);
+                }
             }
         }
     }
 
     for (let p=1; p<=10; p++) {
+        // 같은 거 통계내기 위해 정렬
         // participants[p].startItem.sort((a, b) => {
         //     return a - b;
         // })
         for (let i=0; i<participants[p].itemBuild.length; i++) {
-            console.log(participants[p].itemBuild[i]);
+            console.log(participants[p].itemBuild[i] + '(' + gameinfo.getItem(participants[p].itemBuild[i]).name + ')');
         }
         console.log('======================');
     }
