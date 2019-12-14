1cae1808f6a8fce9e3b100c6ab03c2cba8261ecb
heejun <vsv21gml@naver.com>
2019-12-14 15:41:37 +0900

    game 기본데이터 라이엇 api사용해서 가져오기

--- routes/request.js
+++ routes/request.js
@@ -2,6 +2,7 @@ let request = require("request");
 let conf = require('./../conf.json')
 
 export const GET = (url, callback) => {
-    let makeUrl = conf['url'] + url + '?api_key=' + conf['api_key']
+    let makeUrl = url.indexOf('http') === -1 ? conf['url'] + url + '?api_key=' + conf['api_key'] : url;
+
     request(makeUrl, callback);
 }
--- utils/gameinfo.js
+++ utils/gameinfo.js
@@ -1,12 +1,35 @@
-const parserJsonToObject = (data, key) => {
-    let object = {};
-    for(let jsonData of data)   object[jsonData[key]] = jsonData;
-    return object;
-}
+import {GET} from '../routes/request.js'
 
-const spell = parserJsonToObject(require('./spell.json'),'key');
-const item = parserJsonToObject(require('./item.json'),'id');
-const champ = parserJsonToObject(require('./spell.json'),'key');
+const url = 'https://ddragon.leagueoflegends.com'
+const language = 'ko_KR';
+
+let champ,item,spell;
+
+GET(`${url}/api/versions.json`, function(error, response, body) {
+    const version = JSON.parse(body)[0];
+    console.log(`leagueoflegends api version - ${version}`);
+
+    GET(`${url}/cdn/${version}/data/${language}/item.json`, function(error, response, body) {
+        item = JSON.parse(body).data;
+        console.log('succes item data');
+    })
+    GET(`${url}/cdn/${version}/data/${language}/champion.json`, function(error, response, body) {
+        let champData = JSON.parse(body).data;
+        champ = {};
+        for(let key in champData){
+            champ[champData[key].key] = champData[key];
+        }
+        console.log('succes champion data');
+    })
+    GET(`${url}/cdn/${version}/data/${language}/summoner.json`, function(error, response, body) {
+        let spellData = JSON.parse(body).data;
+        spell = {};
+        for(let key in spellData){
+            spell[spellData[key].key] = spellData[key];
+        }
+        console.log('succes spell data');
+    })
+})
 
 export const getSpell = (id) => {
     return spell[id];
